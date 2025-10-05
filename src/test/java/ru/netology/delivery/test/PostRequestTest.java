9f843dfc80366ea4b3b9478d5ec8d5a1e45a6ef9
alekseyp555 <park.aleksey@gmail.com>
2025-10-05 19:53:35 +0300

    disable api test

--- src/test/java/ru/netology/delivery/test/PostRequestTest.java
+++ src/test/java/ru/netology/delivery/test/PostRequestTest.java
@@ -1,59 +1,59 @@
-package ru.netology.delivery.test;
-
-import com.microsoft.playwright.*;
-import com.microsoft.playwright.options.RequestOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import java.nio.charset.StandardCharsets;
-
-public class PostRequestTest {
-
-    private static Playwright playwright;
-    private BrowserContext context;
-
-    @BeforeClass
-    public void beforeClass() {
-        playwright = Playwright.create();
-        BrowserType browserType = playwright.chromium();
-        Browser browser = browserType.launch();
-        context = browser.newContext();
-    }
-
-    @AfterClass
-    public void afterClass() {
-        context.close();
-        playwright.close();
-    }
-
-    @Test
-    public void postUserDataTest() {
-        // Тело запроса в формате JSON
-        String payload = "{\n" +
-                "    \"id\": 0,\n" +
-                "    \"username\": \"string\",\n" +
-                "    \"firstName\": \"string\",\n" +
-                "    \"lastName\": \"string\",\n" +
-                "    \"email\": \"string\",\n" +
-                "    \"password\": \"string\",\n" +
-                "    \"phone\": \"string\",\n" +
-                "    \"userStatus\": 0\n" +
-                "}";
-
-        // Отправляем POST-запрос с новыми аргументами
-        APIResponse response = context.request().post(
-                "https://petstore.swagger.io/v2/user",
-                RequestOptions.create()
-                        .setHeader("Content-Type", "application/json") // устанавливаем заголовок
-                        .setData(payload));
-
-        // Проверяем статус ответа
-        assert (response.status() == 200 || response.status() == 201); // ожидаем успешный статус
-
-        // Получаем тело ответа и выводим его
-        byte[] bodyBytes = response.body();
-        String bodyString = new String(bodyBytes, StandardCharsets.UTF_8);
-        System.out.println("Response body: " + bodyString);
-    }
-}
+//package ru.netology.delivery.test;
+//
+//import com.microsoft.playwright.*;
+//import com.microsoft.playwright.options.RequestOptions;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.Test;
+//
+//import java.nio.charset.StandardCharsets;
+//
+//public class PostRequestTest {
+//
+//    private static Playwright playwright;
+//    private BrowserContext context;
+//
+//    @BeforeClass
+//    public void beforeClass() {
+//        playwright = Playwright.create();
+//        BrowserType browserType = playwright.chromium();
+//        Browser browser = browserType.launch();
+//        context = browser.newContext();
+//    }
+//
+//    @AfterClass
+//    public void afterClass() {
+//        context.close();
+//        playwright.close();
+//    }
+//
+//    @Test
+//    public void postUserDataTest() {
+//        // Тело запроса в формате JSON
+//        String payload = "{\n" +
+//                "    \"id\": 0,\n" +
+//                "    \"username\": \"string\",\n" +
+//                "    \"firstName\": \"string\",\n" +
+//                "    \"lastName\": \"string\",\n" +
+//                "    \"email\": \"string\",\n" +
+//                "    \"password\": \"string\",\n" +
+//                "    \"phone\": \"string\",\n" +
+//                "    \"userStatus\": 0\n" +
+//                "}";
+//
+//        // Отправляем POST-запрос с новыми аргументами
+//        APIResponse response = context.request().post(
+//                "https://petstore.swagger.io/v2/user",
+//                RequestOptions.create()
+//                        .setHeader("Content-Type", "application/json") // устанавливаем заголовок
+//                        .setData(payload));
+//
+//        // Проверяем статус ответа
+//        assert (response.status() == 200 || response.status() == 201); // ожидаем успешный статус
+//
+//        // Получаем тело ответа и выводим его
+//        byte[] bodyBytes = response.body();
+//        String bodyString = new String(bodyBytes, StandardCharsets.UTF_8);
+//        System.out.println("Response body: " + bodyString);
+//    }
+//}
